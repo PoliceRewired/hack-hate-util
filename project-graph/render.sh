ba6fc97be1044cfa341c8e1f84f7779f0faff9f2
Lewis Westbury <lewis@policerewired.org>
2020-10-20 23:11:01 +0100

    Render projects graph

--- project-graph/render.sh
+++ project-graph/render.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-dot -Tpng ./hack-hate-2020-projects.dot -o hack-hate-2020-projects.png
+dot -Tpng ./hack-hate-2020-projects.dot -o hack-hate-2020-projects-and-teams.png
+dot -Tpng ./hack-hate-2020-projects-no-teams.dot -o hack-hate-2020-projects.png
